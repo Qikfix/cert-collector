2376e2bea89d0d2073aeb0eef7d5385ebf61fd2b
Waldirio M Pinheiro <waldirio@gmail.com>
2023-09-18 18:18:36 -0700

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # cert-collector
 Script to collect all the possible certificate information from Satellite, Capsule and Content Host
+
+It still under construction, but the main idea of usage it's something as mentioned below
+
+- On your Satellite Server
+```
+# wget path_here
+# ./cert-collector.sh --server
+```
+- On your Capsule Server
+```
+# wget path_here
+# ./cert-collector.sh --capsule
+```
+- On your Content Host
+```
+# wget path_here
+# ./cert-collector.sh --content_host
+```
+
+This should be enough to collect all the information we need and tell you which file should be shared/uploaded to the Support Case.
---
+++ cert-collector.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+#
+# Disclaimer .: This script doesn't intend to be supported or responsability of Red Hat, this is an
+#               independent initiative that will help for sure any Satellite case related to certs/ssl
+# date .......: 09/18/2023
+# Developer ..: Waldirio M Pinheiro <waldirio@redhat.com>/<waldirio@gmail.com>
+# License ....: GPLv3
+#
+
+
+if [ "$1" == "" ]; then
+  echo "Please, execute '$0 --server', '$0 --capsule' or '$0 --content_host'"
+  echo "exiting ..."
+  exit 0
+fi
+
+
+LOG_DIR="/var/tmp/certs_log"
+
+
+create_log_dir()
+{
+  if [ ! -d $LOG_DIR ]; then
+    echo "Creating the dir $LOG_DIR"
+    mkdir -pv $LOG_DIR
+  else
+    echo "dir $LOG_DIR already around"
+  fi
+}
+
+
+#create_log_dir
+
+server_func()
+{
+  LOG_SRV_FILE="/var/tmp/certs_full_server_info.log"
+  > $LOG_SRV_FILE
+
+  SRV_PORT_SSH="22"
+  SRV_PORT_DB="5432"
+  SRV_PORT_SMART_PROXY="9009"
+  SRV_PORT_REDIS="6379"
+  SRV_PORT_HTTP="80"
+  SRV_PORT_HTTPS="443"
+  SRV_PORT_CP="23443"
+  SRV_SERVER_SSL_CA="/etc/pki/katello/certs/katello-default-ca.crt"
+  SRV_SERVER_SSL_CERT="/etc/pki/katello/certs/katello-apache.crt"
+  SRV_SERVER_ANCHORS_DIR="/etc/pki/ca-trust/source/anchors/"
+
+  echo "# Server" | tee -a $LOG_SRV_FILE
+
+  echo | tee -a $LOG_SRV_FILE
+  echo "# Checking the Apache Cert" | tee -a $LOG_SRV_FILE
+  echo "# echo | openssl s_client -connect $(hostname):$SRV_PORT_HTTPS" | tee -a $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  echo | openssl s_client -connect $(hostname):$SRV_PORT_HTTPS &>> $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+
+  echo | tee -a $LOG_SRV_FILE
+  echo "# Checking the Apache response using - $SRV_SERVER_SSL_CA" | tee -a $LOG_SRV_FILE
+  echo "# curl -vvv --cacert $SRV_SERVER_SSL_CA https://$(hostname):$SRV_PORT_HTTPS" | tee -a $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  curl -vvv --cacert $SRV_SERVER_SSL_CA https://$(hostname):$SRV_PORT_HTTPS &>> $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+
+  echo | tee -a $LOG_SRV_FILE
+  echo "# Checking the Apache response using - $SRV_SERVER_SSL_CERT" | tee -a $LOG_SRV_FILE
+  echo "# curl -vvv --cacert $SRV_SERVER_SSL_CERT https://$(hostname):$SRV_PORT_HTTPS" | tee -a $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  curl -vvv --cacert $SRV_SERVER_SSL_CERT https://$(hostname):$SRV_PORT_HTTPS &>> $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+
+  echo | tee -a $LOG_SRV_FILE
+  echo "# Checking the Candlepin Cert" | tee -a $LOG_SRV_FILE
+  echo "# echo | openssl s_client -connect localhost:$SRV_PORT_CP" | tee -a $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  echo | openssl s_client -connect localhost:$SRV_PORT_CP &>> $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  
+  echo | tee -a $LOG_SRV_FILE
+  echo "# Checking the Candlepin endpoint" | tee -a $LOG_SRV_FILE
+  echo "# curl -s https://localhost:$SRV_PORT_CP/candlepin/status | python3 -m json.tool" | tee -a $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  curl -s https://localhost:$SRV_PORT_CP/candlepin/status | python3 -m json.tool &>> $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+
+  echo | tee -a $LOG_SRV_FILE
+  echo "# Checking the anchors dir" | tee -a $LOG_SRV_FILE
+  echo "# ls -l /etc/pki/ca-trust/source/anchors/" | tee -a $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  ls -l /etc/pki/ca-trust/source/anchors/ &>> $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  
+  echo | tee -a $LOG_SRV_FILE
+  echo "# Checking the certs content under anchors dir" | tee -a $LOG_SRV_FILE
+  echo "# for b in \$(ls -1 $SRV_SERVER_ANCHORS_DIR); do echo - \$b;openssl x509 -in $SRV_SERVER_ANCHORS_DIR/\$b -text -noout; done" | tee -a $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  for b in $(ls -1 $SRV_SERVER_ANCHORS_DIR); do echo - $b;openssl x509 -in $SRV_SERVER_ANCHORS_DIR/$b -text -noout; done &>> $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+
+  echo | tee -a $LOG_SRV_FILE
+  echo "# Checking the certs set in answer file" | tee -a $LOG_SRV_FILE
+  echo "# grep cert /etc/foreman-installer/scenarios.d/satellite-answers.yaml" | tee -a $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  grep cert /etc/foreman-installer/scenarios.d/satellite-answers.yaml &>> $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+
+  echo | tee -a $LOG_SRV_FILE
+  echo "# Checking the bundle certs" | tee -a $LOG_SRV_FILE
+  echo "# awk -v cmd='openssl x509 -noout -subject -issuer -dates' ' /BEGIN/{close(cmd)};{print | cmd}' <  /etc/pki/tls/certs/ca-bundle.crt" | tee -a $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+  awk -v cmd='openssl x509 -noout -subject -issuer -dates' ' /BEGIN/{close(cmd)};{print | cmd}' <  /etc/pki/tls/certs/ca-bundle.crt &>> $LOG_SRV_FILE
+  echo "=====" >> $LOG_SRV_FILE
+
+
+
+  #echo | openssl s_client -connect cdn.redhat.com:443 -CAfile /etc/rhsm/ca/redhat-uep.pem -showcerts
+  #echo | openssl s_client -connect subscription.rhsm.redhat.com:443 -CAfile /etc/rhsm/ca/redhat-uep.pem -showcerts
+  ##nmap --script  ssl-enum-ciphers -p 443 subscription.rhsm.redhat.com
+  #curl -vvv --head --cacert /etc/rhsm/ca/redhat-uep.pem https://subscription.rhsm.redhat.com
+  #curl -vvv --head --cacert /etc/rhsm/ca/redhat-uep.pem https://cdn.redhat.com
+  #echo | openssl s_client -connect subscription.rhsm.redhat.com:443 -CAfile /etc/rhsm/ca/redhat-uep.pem -showcerts -debug
+  #curl -s -v --header "Content-Type:application/json" --request GET --cacert /etc/pki/katello/certs/katello-server-ca.crt https://$(hostname)/katello/api/status | python3 -m json.tool
+
+}
+
+capsule_func()
+{
+  echo "capsule"
+}
+
+ch_func()
+{
+  echo "content host"
+}
+
+case $1 in
+  "--server")
+    server_func
+    ;;
+  "--capsule")
+    capsule_func
+    ;;
+  "--content_host")
+    ch_func
+    ;;
+  *) echo -e "Option not available\nexiting ..."
+    ;;
+esac
